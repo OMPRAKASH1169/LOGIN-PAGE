51b42fcdfd582f837dacc63ce3b74265f5817ae7
OMPRAKASH1169 <108258954+OMPRAKASH1169@users.noreply.github.com>
2022-06-27 13:42:40 +0530

    Add files via upload

---
+++ Form1 CODE LOGIN (20BCA1169).cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOGIN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtusername.Text=="your_user_name" && txtpassword.Text=="your_password")
+            {
+                new Form2().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("The user name and password  you enter is incorrect, try again");
+                txtusername.Clear();
+                txtpassword.Clear();
+                txtusername.Focus();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            txtusername.Clear();
+            txtpassword.Clear();
+            txtusername.Focus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
